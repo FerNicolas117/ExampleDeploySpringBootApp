65fc4ee9ff5fc79a5cd04fc70992a59cec5ad828
Fernando Nicolás <he440185@uaeh.edu.mx>
2024-07-16 02:51:42 -0600

    first commit

---
+++ src/main/java/com/example/deploy/ExampleDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleDeployApplication.class, args);
+	}
+
+}
